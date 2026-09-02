740063e8bb948bb22e1c73cbdfb729382a7ee3c7
Harsha vardhan Avuru <avuruharsha378@gmail.com>
2026-09-02 10:03:34 +0530

    Time: 32 ms (6.94%) | Memory: 47.7 MB (5.03%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,35 +1,35 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         ArrayList<Integer>empty=new ArrayList<>(); 
-        for(int asteroid:asteroids){ 
-            empty.add(asteroid);
-        }  
+        for(int asteriod:asteroids){ 
+            empty.add(asteriod); 
+        } 
         boolean collision=true; 
         while(collision){ 
-            collision=false;
+            collision=false; 
             for(int i=0;i<empty.size()-1;i++){ 
                 int left=empty.get(i); 
                 int right=empty.get(i+1); 
-                while(left>0 && right<0){ 
-                    collision=true; 
+                if(left>0 && right<0){ 
+                    collision=true;
                     if(Math.abs(left)>Math.abs(right)){ 
                         empty.remove(i+1);
-                    } 
+                    }  
                     else if(Math.abs(left)<Math.abs(right)){ 
                         empty.remove(i);
-                    } 
+                    }
                     else{ 
-                        empty.remove(i+1);  
+                        empty.remove(i+1); 
                         empty.remove(i);
-                    } 
+                    }
                     break;
                 }
             }
-        } 
-        int ans[]=new int[empty.size()]; 
+        }
+        int[]res=new int[empty.size()]; 
         for(int i=0;i<empty.size();i++){ 
-            ans[i]=empty.get(i);
-        } 
-        return ans; 
+            res[i]=empty.get(i);
+        }
+        return res; 
     }
 }
