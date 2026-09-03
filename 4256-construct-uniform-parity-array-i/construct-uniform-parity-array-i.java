73e6e119401fe7b667fbfa9f55f7e8d02bc8a834
Harsha vardhan Avuru <avuruharsha378@gmail.com>
2026-09-03 20:29:21 +0530

    Time: 1 ms (32.15%) | Memory: 45.2 MB (58.38%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        boolean oneven=false; 
+        boolean onodd=false; 
+        for(int x:nums1){ 
+            if(x%2==0){ 
+                oneven=true;
+            } 
+            else{ 
+                onodd=false;
+            }
+        }
+        if(!oneven || !onodd){ 
+            return true;
+        } 
         return true; 
     }
 }
