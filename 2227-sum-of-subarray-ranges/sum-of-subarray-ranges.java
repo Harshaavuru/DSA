5de0535e5f579efaeb8842fb89c2ee6d2a888c47
Harsha vardhan Avuru <avuruharsha378@gmail.com>
2026-09-04 16:05:05 +0530

    Time: 16 ms (82.72%) | Memory: 47 MB (19.63%) - LeetSync

---
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        long maxsum=0; 
+        long minsum=0; 
+        int n=nums.length; 
+        Stack<Integer>empty=new Stack<>(); 
+        for(int i=0;i<=n;i++){ 
+            while(!empty.isEmpty() && (i==n || nums[empty.peek()]<nums[i])){ 
+                int index= empty.pop(); 
+                int left=0; 
+                if(empty.isEmpty()){ 
+                    left=index+1; 
+                }
+                else{ 
+                    left=index-empty.peek();
+                } 
+                int right=i-index ;
+                maxsum+=(long) nums[index]*left*right;
+            }
+            if(i<n) empty.push(i);
+        }
+        empty.clear();
+        for(int i=0;i<=n;i++){ 
+            while(!empty.isEmpty() && (i==n || nums[empty.peek()]>nums[i])){ 
+                int index=empty.pop(); 
+                int left=0; 
+                if(empty.isEmpty()){ 
+                    left=index+1; 
+                }
+                else{ 
+                    left=index-empty.peek();
+                }
+                int right=i-index;
+                minsum+=(long) nums[index]*left*right;
+            }
+            if(i<n) empty.add(i);
+        } 
+        return (long) maxsum-minsum;
+    }
+}
